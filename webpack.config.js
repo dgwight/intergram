5480719bd329d0580c33fb2a50b2d51fc77e5a26
wightdylan <dylangwight@gmail.com>
2018-02-26 16:07:23 -0500

    iframe immediately

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist', 'js'),
         filename: '[name].js',
-        publicPath: '/js/'
+        publicPath: '/js/',
     },
     module: {
         loaders: [
